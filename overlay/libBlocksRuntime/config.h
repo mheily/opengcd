6d3afb541274380bb3021a5b49b7303dca396bd1
mheily <mheily@e45b9353-0cb2-43d3-a8f6-a29e18b24d03>
2013-05-02 02:16:08 +0000

    Remove the overlay/ files, they are unused

--- overlay/libBlocksRuntime/config.h
+++
@@ -1,3 +0,0 @@
-/* FIXME: should actually test these */
-#define HAVE_SYNC_BOOL_COMPARE_AND_SWAP_INT 1
-#define HAVE_SYNC_BOOL_COMPARE_AND_SWAP_LONG 1
--- overlay/libpthread_workqueue/config.h
+++
@@ -1,7 +0,0 @@
-/* AUTOMATICALLY GENERATED -- DO NOT EDIT */
-#define HAVE_ERR_H 1
-#define PROGRAM "libpthread_workqueue"
-#define VERSION "0.7"
-#define TARGET "linux"
-#define API "posix"
-#define CFLAGS "-Wall -Wextra -Werror -D_XOPEN_SOURCE=600 -D__EXTENSIONS__ -D_GNU_SOURCE -std=c99 -I./include -I./src -g -O2 -m64 -fpic -fvisibility=hidden -pthread"
